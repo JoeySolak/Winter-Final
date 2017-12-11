21049dadf0d10bfb25a1d6d2fd70146e05160e07
JoeySolak <8jsolak@stpatrick.org>
2017-12-11 09:18:50 -0600

    Add files via upload

--- Device.js
+++ Device.js
@@ -25,6 +25,7 @@ function Device(t,ma,c){
     this.wake = function(){
         if((this.state == "idle" || this.state == "off") && this.juice>0){
         this.state = "active";
+      }
     }
     this.sleep = function(){
         if(this.state == "active" && this.juice>0){
@@ -45,19 +46,35 @@ function Device(t,ma,c){
             let charge = (this.millAmps / this.capacity);
             let output = 1 - this.rate[1];
             let time = min / 60;
-            this.juice = this.juice + charge*output*time*0.9765;
+            this.juice = this.juice + charge*output*time;
         }
         else if(this.state = "active"){
             let charge = (this.millAmps / this.capacity);
             let output = 1 - this.rate[2];
             let time = min / 60;
-            this.juice = this.juice + charge*output*time*0.77;
+            this.juice = this.juice + charge*output*time;
         }
-}
         //resets juice to 1 if it has exceeded 1
         if(this.juice>1){
           this.juice=1;
-      };
+      }
+};
+
+  this.use = function(min){
+    let time = min/60
+
+    if(this.state == "off"){
+      let juice = juice - this.rate[0];
+    }
+    else if(this.state == "idle"){
+      let juice = juice - this.rate[1]
+    }
+    if(this.state == "active"){
+      let juice = juice - this.rate[2]
+    }
+  }
+
+}
 
 
 //end of the device declaration
